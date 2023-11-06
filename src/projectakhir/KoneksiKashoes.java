17ccfc5a2e595d55c595756c99a2715ecd50f564
Diaz14041 <diaznura@outlook.com>
2023-11-06 14:09:07 +0700

    cleanup koneksi
    Signed-off-by: Diaz14041 <diaznura@outlook.com>

--- src/projectakhir/KoneksiKashoes.java
+++ src/projectakhir/KoneksiKashoes.java
@@ -4,40 +4,27 @@
  */
 package projectakhir;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import javax.swing.JOptionPane;
+
 /**
  *
  * @author LENOVO
  */
 public class KoneksiKashoes {
 
-   
-    Connection con;
-    Statement st;
-    public static Connection koneksikashoesdB(){
-     try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection koneksi = DriverManager.getConnection("jdbc:mysql://localhost/kashoes","root","arrosyid028");
-            
+    public static Connection koneksi;
+
+    public static Connection koneksikashoesdB() {
+        String url = "jdbc:mysql://localhost:3306/kashoes";
+        String user = "root";
+        String pass = "diaz";
+        try {
+            koneksi = DriverManager.getConnection(url, user, pass);
             return koneksi;
-        }
-        catch(ClassNotFoundException | SQLException e){
+        } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, e);
-            return null; 
-        }
-    }
-    private void config(){
-         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection koneksi = DriverManager.getConnection("jdbc:mysql://localhost/kashoes","root","arrosyid028");
-            st = con.createStatement();
-        }
-        catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Koneksi gagal"+e.getMessage());
+            return null;
         }
     }
 }
